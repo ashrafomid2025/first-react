e09288959170e3506236318f5739e70cece895aa
ashrafomid2025 <omidmashraf@gmail.com>
2025-04-10 21:22:35 +0430

    chages

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -2,6 +2,12 @@ function Hello() {
   return (
     <div>
       <h1>In the name of Allah</h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        quibusdam aut accusantium voluptatum at, possimus provident et amet
+        repudiandae doloribus atque tenetur hic odit repellat quas aperiam unde
+        doloremque alias?
+      </h2>
     </div>
   );
 }
