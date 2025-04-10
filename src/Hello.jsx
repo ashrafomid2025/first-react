21a6ee8bff401bbb902d70852a002941f256648a
ashrafomid2025 <omidmashraf@gmail.com>
2025-04-10 21:24:07 +0430

    changes

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -2,12 +2,6 @@ function Hello() {
   return (
     <div>
       <h1>In the name of Allah</h1>
-      <h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        quibusdam aut accusantium voluptatum at, possimus provident et amet
-        repudiandae doloribus atque tenetur hic odit repellat quas aperiam unde
-        doloremque alias?
-      </h2>
     </div>
   );
 }
