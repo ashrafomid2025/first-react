549bd31343d9552d4c9f961f51ca930fe9965617
ashrafomid2025 <omidmashraf@gmail.com>
2025-06-12 18:37:08 +0430

    react changes

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,7 +2,7 @@ function Hello(props) {
   return (
     <div>
       <h1>
-        {props.person.name} {props.person.emoji}- {props.person.message}- your
+        {props.person.name} {props.person.emoji}: {props.person.message}- your
         seatnumbers goes like this {props.person.seatNumbers}
       </h1>
     </div>
