996d057091fcc199ca8f6408f2ee789cbac00b56
ashrafomid2025 <omidmashraf@gmail.com>
2025-06-13 16:39:54 +0430

    first commit

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,11 @@
 function Hello(props) {
   return (
-    <div>
+    <>
       <h1>
         {props.person.name} {props.person.emoji}: {props.person.message}- your
         seatnumbers goes like this {props.person.seatNumbers}
       </h1>
-    </div>
+    </>
   );
 }
 export default Hello;
