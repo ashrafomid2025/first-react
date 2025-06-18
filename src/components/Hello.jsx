fd51742151e18c44aaced05f73f3e7fcc7d6c6c7
ashrafomid2025 <omidmashraf@gmail.com>
2025-06-18 23:10:50 +0430

    changes in index file

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -5,7 +5,7 @@ function Hello(props) {
         {props.person.name} {props.person.emoji}: {props.person.message}- your
         here is the seat number stored in array: {props.person.seatNumbers}
       </h1>
-      <h2>Hi there, I am commig from hello jsx</h2>
+      <h2>Hi there</h2>
     </>
   );
 }
